c45ba74c356ecb86020a97ddb247154967da9a97
Alban Ramette <alban.ramette@ifik.unibe.ch>
2019-08-02 17:52:03 +0200

    Update config.pm

--- 1.8/lib/config.pm
+++ 1.8/lib/config.pm
@@ -23,31 +23,30 @@ $VERSION     = 1.08;
 					$iqtree
 					$TaxDictFile
 					%RefDB
-
-					
+			
 );   		
-
-#####################################################        
-#  Edit as needed the sections below only ##########        
-###################################################        
-our $LIBDIRECTORY	="/software/KM/Transfer/cron_lorcan/v1.8.1/lib"; #<-------
-######
-our $PERL		="/software/KM/perl5/perlbrew/perls/perl-5.28.0/bin/perl";
-our $SEQKIT		="/software_conda/3/pkgs/seqkit-0.8.0-0/bin/seqkit";
-our $PORECHOP		="/software_conda/3/bin/porechop";		
-our $minimap2		="module load MultipleSequenceAlignment/minimap2/2.5-r601-dirty;minimap2";		
-our $samtools		="module load UHTS/samtools/1.4;samtools";		
-our $BLASTN		="module load Blast/blast/2.6.0;blastn";
-our $Rscript 		="/software/KM/R-3.5.0_KM_WGS/bin/Rscript";
-our $mafft 		="/software_conda/3/bin/mafft";
-our $Gblocks		="/software_conda/3/bin/Gblocks";
-our $iqtree		="/software_conda/3/bin/iqtree";
-######
-our $TaxDictFile	="/software/KM/Transfer/cron_lorcan/v1.8/LeBiBiRef.taxdict.csv"; 
-######
+#########################################################################
+#########################################################################
+#  Edit as needed the paths below as needed (see README.MD for instructions):
+############################################# 
+our $LIBDIRECTORY ="/home/alban/test_software/LORCAN_install_test/LORCAN/1.8/lib"; #<-------
+############
+our $PERL	="/home/alban/.conda/envs/LORCAN-env/bin/perl";
+our $SEQKIT	="/home/alban/.conda/envs/LORCAN-env/bin/seqkit";
+our $PORECHOP	="/home/alban/.conda/envs/LORCAN-env/bin/porechop";		
+our $minimap2	="/home/alban/.conda/envs/LORCAN-env/bin/minimap2";		
+our $samtools	="/home/alban/.conda/envs/LORCAN-env/bin/samtools";		
+our $BLASTN	="/home/alban/.conda/envs/LORCAN-env/bin/blastn";
+our $Rscript 	="/software_conda/3/bin/Rscript";
+our $mafft 	="/home/alban/.conda/envs/LORCAN-env/bin/mafft";
+our $Gblocks	="/home/alban/.conda/envs/LORCAN-env/bin/Gblocks";
+our $iqtree	="/home/alban/.conda/envs/LORCAN-env/bin/iqtree";
+##########
+our $TaxDictFile="/home/alban/test_software/LORCAN_install_test/LORCAN/DB/16S/BiBi/My16S_taxdict.csv";
+##########
 our %RefDB=(
-	BiBi16S => "/storage/Analyses/Pipelines/dev/LORCAN/v1.8/leBiBicustomDB/16S_stringent_custom.fasta", 
-	BiBi16SLong => "/storage/databases/leBIBI/SSU-rDNA/002_long16S_Amplicon/16S_stringent_custom_long.fasta", 
-	ADV => "/storage/databases/adenovirus/2018_10_10/Human_Adenovirus_genomes_20181010.fasta",
+	My16SDB => "/home/alban/test_software/LORCAN_install_test/LORCAN/DB/16S/BiBi/16S_stringent_custom.fasta", 
+	MyOtherGeneDB => "/home/alban/databases/adenovirus/2018_10_10/Human_Adenovirus_genomes_20181010.fasta",
 );
+####################################################
 1;
